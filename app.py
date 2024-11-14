057e541213c84505bfbd31b091d97e028da99639
Guillermo Martín <71447702+gmartiin95@users.noreply.github.com>
2024-11-14 15:52:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,23 +21,12 @@ if ciudades_seleccionadas:
     df_filtrado_por_ciudad = df[df['City'].isin(ciudades_seleccionadas)]
     codigos_postales_disponibles = df_filtrado_por_ciudad['Zipcode'].unique()
 else:
-    # Si no se selecciona ninguna ciudad, usar todos los códigos postales
+    # Si no se selecciona ninguna ciudad, mostrar todos los códigos postales
     codigos_postales_disponibles = df['Zipcode'].unique()
 
-# Convertir los códigos postales disponibles a texto para mostrar en el placeholder
-placeholder_codigos_postales = ", ".join(map(str, codigos_postales_disponibles))
-
-# Bloque 2: Filtro por Códigos Postales (como entrada de texto)
-st.header("Filtro por Códigos Postales")
-st.write("Ingresa los códigos postales separados por comas (Ejemplo: 4006, 4004, 37004)")
-input_zipcodes = st.text_input('Códigos postales', placeholder_codigos_postales)  # Mostrar solo los códigos relevantes
-
-# Convertir los códigos postales ingresados en una lista de enteros
-try:
-    zipcodes_a_buscar = [int(zipcode.strip()) for zipcode in input_zipcodes.split(',')]
-except ValueError:
-    st.write("Por favor, asegúrate de que todos los códigos postales sean números separados por comas.")
-    zipcodes_a_buscar = []
+# Bloque 2: Filtro por Código Postal (dependiente del filtro de ciudad)
+st.header("Filtro por Código Postal")
+codigos_postales_seleccionados = st.multiselect("Selecciona uno o varios códigos postales", codigos_postales_disponibles)
 
 # Filtrar el DataFrame en función de los filtros seleccionados
 df_filtrado_final = df.copy()
@@ -46,9 +35,9 @@ df_filtrado_final = df.copy()
 if ciudades_seleccionadas:
     df_filtrado_final = df_filtrado_final[df_filtrado_final['City'].isin(ciudades_seleccionadas)]
 
-# Aplicar filtro de código postal si hay códigos postales ingresados
-if zipcodes_a_buscar:
-    df_filtrado_final = df_filtrado_final[df_filtrado_final['Zipcode'].isin(zipcodes_a_buscar)]
+# Aplicar filtro de código postal si hay códigos postales seleccionados
+if codigos_postales_seleccionados:
+    df_filtrado_final = df_filtrado_final[df_filtrado_final['Zipcode'].isin(codigos_postales_seleccionados)]
 
 # Seleccionar solo las columnas que necesitas para el resultado
 df_resultado = df_filtrado_final[['Zipcode', 'Frame id', 'Full address', 'Publisher', 'City', 'Venue types']]
