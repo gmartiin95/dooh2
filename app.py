1d0601e7eca2c5097b671c8390e2a3e2969f050a
Guillermo Martín <71447702+gmartiin95@users.noreply.github.com>
2024-11-14 15:20:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ df = pd.read_csv(ruta_csv)
 df.fillna(0, inplace=True)
 df['Zipcode'] = df['Zipcode'].astype(float).astype(int)
 
-# Paso 3: Crear una interfaz para que el usuario ingrese los códigos postales
-st.title('Búsqueda de Códigos Postales')
+st.title('Búsqueda de Información')
+
+# Bloque 1: Búsqueda por Códigos Postales
+st.header("Búsqueda por Códigos Postales")
 st.write("Ingresa los códigos postales separados por comas (Ejemplo: 4006, 4004, 37004)")
 
 # Caja de texto para ingresar los códigos postales
@@ -19,16 +21,35 @@ input_zipcodes = st.text_input('Códigos postales', '4006, 4004, 37004')  # Valo
 # Convertir los códigos postales ingresados en una lista de enteros
 zipcodes_a_buscar = [int(zipcode.strip()) for zipcode in input_zipcodes.split(',')]
 
-# Paso 4: Filtrar el DataFrame para obtener solo las filas con códigos postales que coinciden
-df_filtrado = df[df['Zipcode'].isin(zipcodes_a_buscar)]
+# Filtrar el DataFrame por códigos postales
+df_filtrado_zip = df[df['Zipcode'].isin(zipcodes_a_buscar)]
 
-# Paso 5: Seleccionar solo las columnas que necesitas
-df_resultado = df_filtrado[['Zipcode', 'Frame id', 'Full address', 'Publisher', 'City', 'Venue types']]
+# Seleccionar solo las columnas que necesitas para el resultado
+df_resultado_zip = df_filtrado_zip[['Zipcode', 'Frame id', 'Full address', 'Publisher', 'City', 'Venue types']]
 
-# Paso 6: Mostrar el resultado en una tabla
-if not df_resultado.empty:
-    st.write("Resultados encontrados:")
-    st.dataframe(df_resultado)
+# Mostrar resultados del bloque de códigos postales
+if not df_resultado_zip.empty:
+    st.write("Resultados encontrados por códigos postales:")
+    st.dataframe(df_resultado_zip)
 else:
     st.write("No se encontraron resultados para los códigos postales ingresados.")
 
+# Bloque 2: Búsqueda por Ciudad
+st.header("Búsqueda por Ciudad")
+
+# Crear un menú desplegable para seleccionar la ciudad
+ciudades_disponibles = df['City'].unique()  # Lista de ciudades únicas en el DataFrame
+ciudad_seleccionada = st.selectbox('Selecciona una ciudad', ciudades_disponibles)
+
+# Filtrar el DataFrame por la ciudad seleccionada
+df_filtrado_ciudad = df[df['City'] == ciudad_seleccionada]
+
+# Seleccionar solo las columnas que necesitas para el resultado
+df_resultado_ciudad = df_filtrado_ciudad[['Zipcode', 'Frame id', 'Full address', 'Publisher', 'City', 'Venue types']]
+
+# Mostrar resultados del bloque de ciudades
+if not df_resultado_ciudad.empty:
+    st.write(f"Resultados encontrados para la ciudad: {ciudad_seleccionada}")
+    st.dataframe(df_resultado_ciudad)
+else:
+    st.write("No se encontraron resultados para la ciudad seleccionada.")
