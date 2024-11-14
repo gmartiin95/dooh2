8d81d9a0afab6c6ba5a4bbd7056587556d434f1c
Guillermo Martín <71447702+gmartiin95@users.noreply.github.com>
2024-11-14 15:34:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,27 @@ df.fillna(0, inplace=True)
 df['Zipcode'] = df['Zipcode'].astype(float).astype(int)
 
 st.title('Búsqueda de Información')
+# Bloque 2: Búsqueda por Ciudad (Elección Múltiple)
+st.header("Búsqueda por Ciudad")
 
+# Crear un campo de selección múltiple para las ciudades
+ciudades_disponibles = df['City'].unique()  # Lista de ciudades únicas en el DataFrame
+ciudades_seleccionadas = st.multiselect('Selecciona una o varias ciudades', ciudades_disponibles)
+
+# Filtrar el DataFrame por las ciudades seleccionadas (si hay alguna seleccionada)
+if ciudades_seleccionadas:
+    df_filtrado_ciudad = df[df['City'].isin(ciudades_seleccionadas)]
+    df_resultado_ciudad = df_filtrado_ciudad[['Zipcode', 'Frame id', 'Full address', 'Publisher', 'City', 'Venue types']]
+
+    # Mostrar resultados del bloque de ciudades
+    if not df_resultado_ciudad.empty:
+        st.write(f"Resultados encontrados para las ciudades seleccionadas:")
+        st.dataframe(df_resultado_ciudad)
+    else:
+        st.write("No se encontraron resultados para las ciudades seleccionadas.")
+else:
+    st.write("Por favor, selecciona al menos una ciudad.")
+    
 # Bloque 1: Búsqueda por Códigos Postales
 st.header("Búsqueda por Códigos Postales")
 st.write("Ingresa los códigos postales separados por comas (Ejemplo: 4006, 4004, 37004)")
@@ -33,24 +53,23 @@ if not df_resultado_zip.empty:
     st.dataframe(df_resultado_zip)
 else:
     st.write("No se encontraron resultados para los códigos postales ingresados.")
-
-# Bloque 2: Búsqueda por Ciudad (Elección Múltiple)
-st.header("Búsqueda por Ciudad")
+# Bloque 3: Búsqueda por venue_type (Elección Múltiple)
+st.header("Búsqueda por Venue type")
 
 # Crear un campo de selección múltiple para las ciudades
-ciudades_disponibles = df['City'].unique()  # Lista de ciudades únicas en el DataFrame
-ciudades_seleccionadas = st.multiselect('Selecciona una o varias ciudades', ciudades_disponibles)
+tipo_disponibles = df['Venue type'].unique()  # Lista de ciudades únicas en el DataFrame
+tipos_seleccionadas = st.multiselect('Selecciona una o varios Venues', tipo_disponibles)
 
 # Filtrar el DataFrame por las ciudades seleccionadas (si hay alguna seleccionada)
-if ciudades_seleccionadas:
-    df_filtrado_ciudad = df[df['City'].isin(ciudades_seleccionadas)]
-    df_resultado_ciudad = df_filtrado_ciudad[['Zipcode', 'Frame id', 'Full address', 'Publisher', 'City', 'Venue types']]
+if tipos_seleccionadas:
+    df_filtrado_tipo= df[df['Venue type'].isin(tipos_seleccionadas)]
+    df_resultado_tipo = df_filtrado_tipo[['Zipcode', 'Frame id', 'Full address', 'Publisher', 'City', 'Venue types']]
 
     # Mostrar resultados del bloque de ciudades
-    if not df_resultado_ciudad.empty:
-        st.write(f"Resultados encontrados para las ciudades seleccionadas:")
-        st.dataframe(df_resultado_ciudad)
+    if not df_resultado_tipo.empty:
+        st.write(f"Resultados encontrados para los Venues seleccionados:")
+        st.dataframe(df_resultado_tipo)
     else:
-        st.write("No se encontraron resultados para las ciudades seleccionadas.")
+        st.write("No se encontraron resultados para los Venues seleccionadas.")
 else:
-    st.write("Por favor, selecciona al menos una ciudad.")
+    st.write("Por favor, selecciona al menos un Venue.")
